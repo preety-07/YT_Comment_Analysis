90ec628196d813ccffdc8dd6441ce6aab75704c5
preety-07 <aayu.1098@gmail.com>
2025-12-16 15:14:54 +0530

    register model to mlflow registry

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -6,7 +6,7 @@ import logging
 import os
 
 # Set up MLflow tracking URI
-mlflow.set_tracking_uri("http://ec2-54-196-109-131.compute-1.amazonaws.com:5000/")
+mlflow.set_tracking_uri("http://ec2-18-61-235-88.ap-south-2.compute.amazonaws.com:5000/")
 
 
 # logging configuration
